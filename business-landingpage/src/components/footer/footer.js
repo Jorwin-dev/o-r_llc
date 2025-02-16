4403a8679e81b7eea3e0bc50e90b53afec09eab0
Jorwin Reyes <jorwin2002@gmail.com>
2025-02-16 11:43:51 -0500

    Footer alterations

--- business-landingpage/src/components/footer/footer.js
+++ business-landingpage/src/components/footer/footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFacebookF, faInstagram, faHouzz } from '@fortawesome/free-brands-svg-icons'
+//import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
@@ -8,9 +11,15 @@ const Footer = () => {
         <p>Copyright © 2025 | All rights reserved.</p>
       </div>
       <div className='footer-right'>
-        <a href='https://www.facebook.com/ORlandscapingconstruction' target='_blank' rel='noopener noreferrer'>FB</a>
-        <a href='https://www.instagram.com/or.landscaping/' target='_blank' rel='noopener noreferrer'>IG</a>
-        <a href='https://www.homeadvisor.com/rated.ORLandscaping.114362277.html' target='_blank' rel='noopener noreferrer'>HA</a>
+        <a href='https://www.facebook.com/ORlandscapingconstruction' target='_blank' rel='noopener noreferrer'>
+          <FontAwesomeIcon icon={faFacebookF}/>
+        </a>
+        <a href='https://www.instagram.com/or.landscaping/' target='_blank' rel='noopener noreferrer'>
+          <FontAwesomeIcon icon={faInstagram}/>
+        </a>
+        <a href='https://www.homeadvisor.com/rated.ORLandscaping.114362277.html' target='_blank' rel='noopener noreferrer'>
+          <FontAwesomeIcon icon={faHouzz}/>
+        </a>
       </div>
     </footer>
   );
