1cf18cde650f6c2c6386bc8aa79b776d4e090a8b
Jorwin Reyes <jorwin2002@gmail.com>
2025-02-15 19:52:31 -0500

    Footer integration

--- business-landingpage/src/components/footer/footer.js
+++ business-landingpage/src/components/footer/footer.js
@@ -4,7 +4,14 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2023 Landscaping Business. All rights reserved.</p>
+      <div className='footer-left'>
+        <p>Copyright © 2025 | All rights reserved.</p>
+      </div>
+      <div className='footer-right'>
+        <a href='https://www.facebook.com/ORlandscapingconstruction' target='_blank' rel='noopener noreferrer'>FB</a>
+        <a href='https://www.instagram.com/or.landscaping/' target='_blank' rel='noopener noreferrer'>IG</a>
+        <a href='https://www.homeadvisor.com/rated.ORLandscaping.114362277.html' target='_blank' rel='noopener noreferrer'>HA</a>
+      </div>
     </footer>
   );
 };
