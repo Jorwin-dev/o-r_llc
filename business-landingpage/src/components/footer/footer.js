de31f718eae3cdef0dfc69b83a60fad1ddeda074
Jorwin Reyes <jorwin2002@gmail.com>
2025-02-17 16:19:12 -0500

    Footer rework

--- business-landingpage/src/components/footer/footer.js
+++ business-landingpage/src/components/footer/footer.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faFacebookF, faInstagram, faHouzz } from '@fortawesome/free-brands-svg-icons'
-//import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import logoWithPhone from '../../assets/O&R_Logo_w_number.png'
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className='footer-left'>
-        <p>Copyright © 2025 | All rights reserved.</p>
+          <a href='/home'>Copyright © 2025</a> 
+          <p> | </p>
+          <a href='https://www.linkedin.com/in/jorwin-reyes-191b26257/' target='_blank' rel='noopener noreferrer' className='footer-link'> 
+          Site Design 
+          </a> 
+          <p> | </p> 
+          <a href='/contact' className='footer-link'> 
+          Contact 
+          </a>
+      </div>
+
+      <div className='footer-center'>
+        <a href='tel:7818390134'>
+          <img src={logoWithPhone} alt='O&R Landscaping & Construction Phone Logo' className='footer-logo '/>
+        </a>
       </div>
       <div className='footer-right'>
         <a href='https://www.facebook.com/ORlandscapingconstruction' target='_blank' rel='noopener noreferrer'>
