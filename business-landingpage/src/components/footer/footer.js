f52470e7a3e8532e45c9f30fb023be080a86d35f
Jorwin Reyes <jorwin2002@gmail.com>
2025-02-19 21:57:18 -0500

    Gallery and Footer alterations

--- business-landingpage/src/components/footer/footer.js
+++ business-landingpage/src/components/footer/footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faFacebookF, faInstagram, faHouzz } from '@fortawesome/free-brands-svg-icons'
+import {faFacebookF, faInstagram, faHouzz, faYelp } from '@fortawesome/free-brands-svg-icons'
+import { faThumbtack } from '@fortawesome/free-solid-svg-icons';
 import logoWithPhone from '../../assets/O&R_Logo_w_number.png'
 
 const Footer = () => {
@@ -25,11 +26,17 @@ const Footer = () => {
         </a>
       </div>
       <div className='footer-right'>
+        <a href='https://www.instagram.com/or.landscaping/' target='_blank' rel='noopener noreferrer'>
+          <FontAwesomeIcon icon={faInstagram}/>
+        </a>
         <a href='https://www.facebook.com/ORlandscapingconstruction' target='_blank' rel='noopener noreferrer'>
           <FontAwesomeIcon icon={faFacebookF}/>
         </a>
-        <a href='https://www.instagram.com/or.landscaping/' target='_blank' rel='noopener noreferrer'>
-          <FontAwesomeIcon icon={faInstagram}/>
+        <a href='https://www.yelp.com/biz/o-and-r-landscaping-and-construction-newton' target='_blank' rel='noopener noreferrer'>
+          <FontAwesomeIcon icon={faYelp}/>
+        </a>
+        <a href='https://www.thumbtack.com/ma/west-newton/lawn-care/or-landscaping-construction/service/486509951875792922' target='_blank' rel='noopener noreferrer'>
+          <FontAwesomeIcon icon={faThumbtack}/>
         </a>
         <a href='https://www.homeadvisor.com/rated.ORLandscaping.114362277.html' target='_blank' rel='noopener noreferrer'>
           <FontAwesomeIcon icon={faHouzz}/>
--- business-landingpage/src/pages/gallery/gallery.js
+++ business-landingpage/src/pages/gallery/gallery.js
@@ -8,15 +8,50 @@ import img4 from "../../assets/O&R_Fall-4.JPG";
 import img5 from "../../assets/O&R_Fall-5.JPG";
 import img6 from "../../assets/O&R_Fall-6.JPG";
 import img7 from "../../assets/patio_driveway-ext_sodding-1.JPG";
-import img8 from "../../assets/patio_driveway-ext_sodding-1.JPG";
+import img8 from "../../assets/patio_driveway-ext_sodding-2.JPG";
 import img9 from "../../assets/patio_fence1.JPG";
 import img10 from "../../assets/patio_fence2.JPG";
 import img11 from "../../assets/mulching_cleanup.JPG";
+import img12 from "../../assets/groundskeeping1.JPG";
+import img13 from "../../assets/groundskpeeing2.JPG";
+import img14 from "../../assets/driveway_ext_and_mulch1.JPG";
+import img15 from "../../assets/driveway_ext_and_mulch2.JPG";
+import img16 from "../../assets/driveway_ext_and_mulch3.JPG";
+import img17 from "../../assets/banda_nenatural1.JPG";
+import img18 from "../../assets/banda_nenatural2.JPG";
+import img19 from "../../assets/snow_night.JPG";
+import img20 from "../../assets/snow_day.jpg";
 
-const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11];
+const images = [
+  img1, img2, img3, img4, img5, img6, img7, img8, img9, img10,
+  img11, img12, img13, img14, img15, img16, img17, img18, img19, img20
+];
 
 const Gallery = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(null);
+
+  const openLightbox = (index) => {
+    setSelectedImage(images[index]);
+    setCurrentIndex(index);
+  };
+
+  const closeLightbox = () => {
+    setSelectedImage(null);
+    setCurrentIndex(null);
+  };
+
+  const nextImage = () => {
+    const newIndex = (currentIndex + 1) % images.length;
+    setCurrentIndex(newIndex);
+    setSelectedImage(images[newIndex]);
+  };
+
+  const prevImage = () => {
+    const newIndex = (currentIndex - 1 + images.length) % images.length;
+    setCurrentIndex(newIndex);
+    setSelectedImage(images[newIndex]);
+  };
 
   return (
     <div className="gallery-container">
@@ -28,16 +63,37 @@ const Gallery = () => {
             src={image}
             alt={`Project ${index + 1}`}
             className="gallery-image"
-            onClick={() => setSelectedImage(image)}
+            onClick={() => openLightbox(index)}
           />
         ))}
       </div>
 
       {/* Lightbox Modal */}
       {selectedImage && (
-        <div className="lightbox" onClick={() => setSelectedImage(null)}>
+        <div className="lightbox">
+          <span className="close-lightbox" onClick={closeLightbox}>×</span>
+
+          {/* Previous Arrow */}
+          <span className="lightbox-arrow left" onClick={prevImage}>&#10094;</span>
+
+          {/* Main Image */}
           <img src={selectedImage} alt="Expanded View" className="lightbox-image" />
-          <span className="close-lightbox" onClick={() => setSelectedImage(null)}>×</span>
+
+          {/* Next Arrow */}
+          <span className="lightbox-arrow right" onClick={nextImage}>&#10095;</span>
+
+          {/* Thumbnails */}
+          <div className="thumbnail-container">
+            {images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Thumbnail ${index + 1}`}
+                className={`thumbnail ${index === currentIndex ? "active" : ""}`}
+                onClick={() => openLightbox(index)}
+              />
+            ))}
+          </div>
         </div>
       )}
     </div>
