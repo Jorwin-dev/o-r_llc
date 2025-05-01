470bbb8aae76b891d774c695e700e0f80c9515a3
Jorwin Reyes <jorwin2002@gmail.com>
2025-05-01 13:26:07 -0400

    Footer logos rework

--- business-landingpage/src/components/footer/footer.js
+++ business-landingpage/src/components/footer/footer.js
@@ -1,35 +1,23 @@
 import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faInstagram, faHouzz, faYelp, faCcVisa, faCcMastercard, faCcAmex, faCcDiscover } from '@fortawesome/free-brands-svg-icons';
-import { faThumbtack, faMoneyBillWave, faMoneyCheckAlt } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faInstagram, faHouzz, faYelp } from '@fortawesome/free-brands-svg-icons';
+import { faThumbtack } from '@fortawesome/free-solid-svg-icons';
 import logoWithPhone from '../../assets/O&R_Logo_w_number.png';
-import venmoLogo from '../../assets/Venmo_App_Icon.png'; // <-- Your Venmo image
-import zelleLogo from '../../assets/Zelle_App_Icon.png'; // <-- Your Zelle image
+import venmoLogo from '../../assets/Venmo_App_Icon.png';
+import zelleLogo from '../../assets/Zelle_App_Icon.png'; 
+import visaLogo from '../../assets/visa-logo.webp';
+import mastercardLogo from '../../assets/mastercard-logo.png';
+import amexLogo from '../../assets/American-Express-logo.png';
+import discoverLogo from '../../assets/discover-logo.png';
+import cashLogo from '../../assets/cash-logo.png';
+import chequeLogo from '../../assets/cheque-logo.png';
 
 const Footer = () => {
   return (
     <>
       <footer className="footer">
-        <div className='footer-left'>
-          {/* Payment Methods */}
-          <FontAwesomeIcon icon={faCcVisa} size="2x" />
-          <FontAwesomeIcon icon={faCcMastercard} size="2x" />
-          <FontAwesomeIcon icon={faCcAmex} size="2x" />
-          <FontAwesomeIcon icon={faCcDiscover} size="2x" />
-          <FontAwesomeIcon icon={faMoneyBillWave} size="2x" />
-          <FontAwesomeIcon icon={faMoneyCheckAlt} size="2x" />
-          <img src={venmoLogo} alt="Venmo" className="payment-icon1" />
-          <img src={zelleLogo} alt="Zelle" className="payment-icon2" />
-        </div>
-
-        <div className='footer-center'>
-          <a href='tel:7818390134'>
-            <img src={logoWithPhone} alt='O&R Landscaping & Construction Phone Logo' className='footer-logo' />
-          </a>
-        </div>
-
-        <div className='footer-right'>
+      <div className='footer-right'>
           <a href='https://www.instagram.com/or.landscaping/' target='_blank' rel='noopener noreferrer'>
             <FontAwesomeIcon icon={faInstagram} />
           </a>
@@ -45,6 +33,24 @@ const Footer = () => {
           <a href='https://www.homeadvisor.com/rated.ORLandscaping.114362277.html' target='_blank' rel='noopener noreferrer'>
             <FontAwesomeIcon icon={faHouzz} />
           </a>
+        </div>
+      
+        <div className='footer-center'>
+          <a href='tel:7818390134'>
+            <img src={logoWithPhone} alt='O&R Landscaping & Construction Phone Logo' className='footer-logo' />
+          </a>
+        </div>
+
+        <div className='footer-left'>
+          {/* Payment Methods */}
+          <img src={visaLogo} alt="Visa" className="visa-icon" />
+          <img src={mastercardLogo} alt="Mastercard" className="master-icon" />
+          <img src={amexLogo} alt="Amex" className="amex-icon" />
+          <img src={discoverLogo} alt="Discover" className="discover-icon" />
+          <img src={cashLogo} alt="Cash" className="cash-icon" />
+          <img src={chequeLogo} alt="Cheque" className="cheque-icon" />
+          <img src={venmoLogo} alt="Venmo" className="venmo-icon" />
+          <img src={zelleLogo} alt="Zelle" className="zelle-icon" />
         </div>
       </footer>
 
