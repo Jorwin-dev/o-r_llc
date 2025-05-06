0b7c2829beac38f3a15385e2c76e05177724d316
Jorwin Reyes <jorwin2002@gmail.com>
2025-05-05 22:37:33 -0400

    Fix casing for Navbar import

--- business-landingpage/src/App.js
+++ business-landingpage/src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/home/Home';
 import Services from './pages/services/services';
 import Gallery from './pages/gallery/gallery';
-import Contact from './pages/contact/contact';
-import Navbar from './components/navbar/navbar';
-import Footer from './components/footer/footer';
+import Contact from './pages/contact/Contact';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import Reviews from './pages/reviews/Reviews';
 import './App.css'; // Global Styles
 
